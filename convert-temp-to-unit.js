3b48d8a3ccdd7bd920f666cf812915922ee3b8d0
Pseudo <134837403+Pse-do@users.noreply.github.com>
2023-09-02 22:21:02 +0100

    Add files via upload

---
+++ convert-temp-to-unit.js
@@ -0,0 +1,29 @@
+let celRadio = document.getElementById("celsius");
+let farRadio = document.getElementById("farenheit");
+let convertBtn = document.getElementById("convertBtn")
+let textBox = document.getElementById("textBox")
+let convertedVal; 
+convertBtn.onclick = function(){
+    switch(true){
+        case celRadio.checked:
+            convertedVal = convertTempToCelsius(textBox.value);
+            alert("The temperature in celsius is "+convertedVal);
+            break; 
+        case farRadio.checked:
+            convertedVal = convertTempToFarenheit(textBox.value);
+            alert("The temperature in farenheit is "+convertedVal);
+            break; 
+        case textBox.value == "":
+            alert("Please enter a value you wish to convert!");
+            break;
+        case !farRadio.checked || !celRadio.checked:
+            alert("Please select a check box!")
+    }
+}
+function convertTempToFarenheit(value){
+   return (value - 32) * 5/9;
+
+}
+function convertTempToCelsius(value){
+    return (value * 9 / 5) + 32;
+}
